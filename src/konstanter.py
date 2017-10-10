5e270aa94ab973e8068c2a0d0beadec1125ec371
Lukas Gjetting <lukasgjetting@gmail.com>
2017-10-10 19:46:55 +0200

    Tilføj konstanter.py til at holde styr på alle de globale konstanter.

---
+++ src/konstanter.py
@@ -0,0 +1,25 @@
+# Definér 5 globale konstanter, der skal bruges i løbet af spillet.
+navne = ["Noah", "Sofia", "Victor", "Alma", "Oliver", "Emma", "Oscar", "Ella", "William", "Ida", "Lukas", "Freja", "Carl", "Clara", "Malthe", "Anna", "Emil", "Laura", "Alfred", "Olivia"]
+nøgleord = ["OVER", "UNDER", "LIGE PÅ"]
+bogstaver = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+kortVærdiTekst = {
+	1: "Es",
+	2: "To",
+	3: "Tre",
+	4: "Fire",
+	5: "Fem",
+	6: "Seks",
+	7: "Syv",
+	8: "Otte",
+	9: "Ni",
+	10: "Ti",
+	11: "Bonde",
+	12: "Dame",
+	13: "Konge"
+}
+kortKulørTekst = {
+	0: "Hjerter",
+	1: "Ruder",
+	2: "Spar",
+	3: "Klør"
+}
